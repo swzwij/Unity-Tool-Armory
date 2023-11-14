56cb9803ac6b14a88d0db5055c7e74d01fa27ebd
Samuel <70766878+swzwij@users.noreply.github.com>
2023-11-14 22:08:52 +0100

    Added Gameobject,Transform, and Vector Extensions

---
+++ Extensions/GameobjectExtensions.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameobjectExtensions
+{
+    /// <summary>
+    /// Dictionary storing cached components associated with GameObjects for fast retrieval using TryGetCachedComponent method.
+    /// The outer dictionary uses GameObject instances as keys, while the inner dictionary stores components with their type names as keys.
+    /// </summary>
+    private static readonly Dictionary<GameObject, Dictionary<string, Component>> _tryGetCachedComponents = new();
+
+    /// <summary>
+    /// Tries to retrieve a cached component of type T associated with the specified GameObject. 
+    /// If the component is cached, returns it; otherwise, fetches the component, caches it, and returns it.
+    /// </summary>
+    /// <typeparam name="T">The type of component to retrieve.</typeparam>
+    /// <param name="gameObject">The GameObject to retrieve the component from.</param>
+    /// <returns>The cached or newly fetched component of type T.</returns>
+    public static T TryGetCachedComponent<T>(this GameObject gameObject)
+        where T : Component
+    {
+        if (!_tryGetCachedComponents.TryGetValue(gameObject, out Dictionary<string, Component> dict))
+        {
+            dict = new Dictionary<string, Component>();
+            _tryGetCachedComponents.Add(gameObject, dict);
+        }
+
+        string typeName = typeof(T).Name;
+
+        if (dict.TryGetValue(typeName, out Component cachedComponent))
+            return (T)cachedComponent;
+
+        T component = gameObject.GetComponent<T>();
+        dict[typeName] = component;
+        return component;
+    }
+
+    /// <summary>
+    /// Gets the specified component of type T from the GameObject. If the component doesn't exist, adds it and returns it.
+    /// </summary>
+    /// <typeparam name="T">The type of component to get or add.</typeparam>
+    /// <param name="gameObject">The GameObject to get or add the component to.</param>
+    /// <returns>The component of type T.</returns>
+    public static T GetOrAddComponent<T>(this GameObject gameObject) 
+        where T : Component 
+        => gameObject.TryGetComponent<T>(out var component) ? component : gameObject.AddComponent<T>();
+
+    /// <summary>
+    /// Checks if a specified component is attached to the GameObject.
+    /// </summary>
+    /// <typeparam name="T">The type of component to check for.</typeparam>
+    /// <param name="gameObject">The GameObject to check.</param>
+    /// <returns>Whether the component is attached.</returns>
+    public static bool HasComponent<T>(this GameObject gameObject) 
+        where T : Component 
+        => gameObject.TryGetComponent<T>(out _);
+    
+}
---
+++ Extensions/VectorExtensions.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public static class VectorExtensions
+{
+    /// <summary>
+    /// Calculates the square distance between two points in 3D space.
+    /// </summary>
+    /// <param name="a">The first point in 3D space.</param>
+    /// <param name="b">The second point in 3D space.</param>
+    /// <returns>The square distance between points 'a' and 'b'.</returns>
+    public static float SqrDistance(Vector3 a, Vector3 b)
+    {
+        Vector3 c = new(b.x - a.x, b.y - a.y, b.z - a.z);
+        return c.x * c.x + c.y * c.y + c.z * c.z;
+    }
+
+    /// <summary>
+    /// Sets the X axis of a Vector3 to the specified value.
+    /// </summary>
+    /// <param name="a">The original Vector3.</param>
+    /// <param name="x">The new value for the X axis.</param>
+    /// <returns>A new Vector3 with the X axis set to the specified value and the Y and Z axes unchanged.</returns>
+    public static Vector3 SetX(this Vector3 a, float x) => new(x, a.y, a.z);
+
+    /// <summary>
+    /// Sets the Y axis of a Vector3 to the specified value.
+    /// </summary>
+    /// <param name="a">The original Vector3.</param>
+    /// <param name="y">The new value for the Y axis.</param>
+    /// <returns>A new Vector3 with the Y axis set to the specified value and the X and Z axes unchanged.</returns>
+    public static Vector3 SetY(this Vector3 a, float y) => new(a.x, y, a.z);
+
+    /// <summary>
+    /// Sets the Z axis of a Vector3 to the specified value.
+    /// </summary>
+    /// <param name="a">The original Vector3.</param>
+    /// <param name="z">The new value for the Z axis.</param>
+    /// <returns>A new Vector3 with the Z axis set to the specified value and the X and Y axes unchanged.</returns>
+    public static Vector3 SetZ(this Vector3 a, float z) => new(a.x, a.y, z);
+
+}
