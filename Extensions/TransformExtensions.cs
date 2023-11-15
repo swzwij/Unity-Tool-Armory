c72ac55a6ef97c0a114d1d66f3f02b34393a6fc9
Samuel <70766878+swzwij@users.noreply.github.com>
2023-11-15 09:59:09 +0100

    Added namespaces to Gameobject, Transform, and Vector Extensions

--- Extensions/TransformExtensions.cs
+++ Extensions/TransformExtensions.cs
@@ -1,97 +1,100 @@
 using UnityEngine;
 
-public static class TransformExtensions
+namespace swzwij.Extensions
 {
-    /// <summary>
-    /// Destroys all child GameObjects of the specified Transform.
-    /// </summary>
-    /// <param name="transform">The parent Transform whose children will be destroyed.</param>
-    public static void DestroyChildren(this Transform transform)
+    public static class TransformExtensions
     {
-        for (int i = 0; i < transform.childCount; i++)
-            Object.Destroy(transform.GetChild(i).gameObject);
+        /// <summary>
+        /// Destroys all child GameObjects of the specified Transform.
+        /// </summary>
+        /// <param name="transform">The parent Transform whose children will be destroyed.</param>
+        public static void DestroyChildren(this Transform transform)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+                Object.Destroy(transform.GetChild(i).gameObject);
+        }
+
+        /// <summary>
+        /// Resets the position, rotation, and scale of the specified Transform to their default values.
+        /// </summary>
+        /// <param name="transform">The Transform to be reset.</param>
+        public static void ResetTransform(this Transform transform)
+        {
+            transform.position = Vector3.zero;
+            transform.rotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+        }
+
+        /// <summary>
+        /// Sets the X position of a Transform.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="x">The new X position value.</param>
+        public static void SetXPosition(this Transform transform, float x)
+            => transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+
+        /// <summary>
+        /// Sets the Y position of a Transform.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="y">The new Y position value.</param>
+        public static void SetYPosition(this Transform transform, float y)
+            => transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+
+        /// <summary>
+        /// Sets the Z position of a Transform.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="z">The new Z position value.</param>
+        public static void SetZPosition(this Transform transform, float z)
+            => transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
+
+        /// <summary>
+        /// Sets the X rotation of a Transform in degrees.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="x">The new X rotation value in degrees.</param>
+        public static void SetXRotation(this Transform transform, float x)
+            => transform.localRotation = Quaternion.Euler(x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
+
+        /// <summary>
+        /// Sets the Y rotation of a Transform in degrees.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="y">The new Y rotation value in degrees.</param>
+        public static void SetYRotation(this Transform transform, float y)
+            => transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, y, transform.localRotation.eulerAngles.z);
+
+        /// <summary>
+        /// Sets the Z rotation of a Transform in degrees.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="z">The new Z rotation value in degrees.</param>
+        public static void SetZRotation(this Transform transform, float z)
+            => transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, z);
+
+        /// <summary>
+        /// Sets the X scale of a Transform.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="x">The new X scale value.</param>
+        public static void SetXScale(this Transform transform, float x)
+            => transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+
+        /// <summary>
+        /// Sets the Y scale of a Transform.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="y">The new Y scale value.</param>
+        public static void SetYScale(this Transform transform, float y)
+            => transform.localScale = new Vector3(transform.localScale.x, y, transform.localScale.z);
+
+        /// <summary>
+        /// Sets the Z scale of a Transform.
+        /// </summary>
+        /// <param name="transform">The Transform to modify.</param>
+        /// <param name="z">The new Z scale value.</param>
+        public static void SetZScale(this Transform transform, float z)
+            => transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, z);
     }
-
-    /// <summary>
-    /// Resets the position, rotation, and scale of the specified Transform to their default values.
-    /// </summary>
-    /// <param name="transform">The Transform to be reset.</param>
-    public static void ResetTransform(this Transform transform)
-    {
-        transform.position = Vector3.zero;
-        transform.rotation = Quaternion.identity;
-        transform.localScale = Vector3.one;
-    }
-
-    /// <summary>
-    /// Sets the X position of a Transform.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="x">The new X position value.</param>
-    public static void SetXPosition(this Transform transform, float x) 
-        => transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
-
-    /// <summary>
-    /// Sets the Y position of a Transform.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="y">The new Y position value.</param>
-    public static void SetYPosition(this Transform transform, float y)
-        => transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
-
-    /// <summary>
-    /// Sets the Z position of a Transform.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="z">The new Z position value.</param>
-    public static void SetZPosition(this Transform transform, float z)
-        => transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
-
-    /// <summary>
-    /// Sets the X rotation of a Transform in degrees.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="x">The new X rotation value in degrees.</param>
-    public static void SetXRotation(this Transform transform, float x)
-        => transform.localRotation = Quaternion.Euler(x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z);
-
-    /// <summary>
-    /// Sets the Y rotation of a Transform in degrees.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="y">The new Y rotation value in degrees.</param>
-    public static void SetYRotation(this Transform transform, float y)
-        => transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, y, transform.localRotation.eulerAngles.z);
-
-    /// <summary>
-    /// Sets the Z rotation of a Transform in degrees.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="z">The new Z rotation value in degrees.</param>
-    public static void SetZRotation(this Transform transform, float z)
-        => transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, z);
-
-    /// <summary>
-    /// Sets the X scale of a Transform.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="x">The new X scale value.</param>
-    public static void SetXScale(this Transform transform, float x)
-        => transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
-
-    /// <summary>
-    /// Sets the Y scale of a Transform.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="y">The new Y scale value.</param>
-    public static void SetYScale(this Transform transform, float y)
-        => transform.localScale = new Vector3(transform.localScale.x, y, transform.localScale.z);
-
-    /// <summary>
-    /// Sets the Z scale of a Transform.
-    /// </summary>
-    /// <param name="transform">The Transform to modify.</param>
-    /// <param name="z">The new Z scale value.</param>
-    public static void SetZScale(this Transform transform, float z)
-        => transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, z);
 }
--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -1,56 +1,59 @@
 using UnityEngine;
 
-public static class VectorExtensions
+namespace swzwij.Extensions
 {
-    /// <summary>
-    /// Calculates the square distance between two points in 3D space.
-    /// </summary>
-    /// <param name="a">The first point in 3D space.</param>
-    /// <param name="b">The second point in 3D space.</param>
-    /// <returns>The square distance between points 'a' and 'b'.</returns>
-    public static float SqrDistance(Vector3 a, Vector3 b)
+    public static class VectorExtensions
     {
-        Vector3 c = new(b.x - a.x, b.y - a.y, b.z - a.z);
-        return c.x * c.x + c.y * c.y + c.z * c.z;
+        /// <summary>
+        /// Calculates the square distance between two points in 3D space.
+        /// </summary>
+        /// <param name="a">The first point in 3D space.</param>
+        /// <param name="b">The second point in 3D space.</param>
+        /// <returns>The square distance between points 'a' and 'b'.</returns>
+        public static float SqrDistance(Vector3 a, Vector3 b)
+        {
+            Vector3 c = new(b.x - a.x, b.y - a.y, b.z - a.z);
+            return c.x * c.x + c.y * c.y + c.z * c.z;
+        }
+
+        /// <summary>
+        /// Sets the X axis of a Vector3 to the specified value.
+        /// </summary>
+        /// <param name="a">The original Vector3.</param>
+        /// <param name="x">The new value for the X axis.</param>
+        /// <returns>A new Vector3 with the X axis set to the specified value and the Y and Z axes unchanged.</returns>
+        public static Vector3 SetX(this Vector3 a, float x) => new(x, a.y, a.z);
+
+        /// <summary>
+        /// Sets the Y axis of a Vector3 to the specified value.
+        /// </summary>
+        /// <param name="a">The original Vector3.</param>
+        /// <param name="y">The new value for the Y axis.</param>
+        /// <returns>A new Vector3 with the Y axis set to the specified value and the X and Z axes unchanged.</returns>
+        public static Vector3 SetY(this Vector3 a, float y) => new(a.x, y, a.z);
+
+        /// <summary>
+        /// Sets the Z axis of a Vector3 to the specified value.
+        /// </summary>
+        /// <param name="a">The original Vector3.</param>
+        /// <param name="z">The new value for the Z axis.</param>
+        /// <returns>A new Vector3 with the Z axis set to the specified value and the X and Y axes unchanged.</returns>
+        public static Vector3 SetZ(this Vector3 a, float z) => new(a.x, a.y, z);
+
+        /// <summary>
+        /// Sets the X axis of a Vector2 to the specified value.
+        /// </summary>
+        /// <param name="a">The original Vector2.</param>
+        /// <param name="x">The new value for the X axis.</param>
+        /// <returns>A new Vector2 with the X axis set to the specified value and the Y axis unchanged.</returns>
+        public static Vector2 SetX(this Vector2 a, float x) => new(x, a.y);
+
+        /// <summary>
+        /// Sets the Y axis of a Vector2 to the specified value.
+        /// </summary>
+        /// <param name="a">The original Vector2.</param>
+        /// <param name="y">The new value for the Y axis.</param>
+        /// <returns>A new Vector2 with the Y axis set to the specified value and the X axis unchanged.</returns>
+        public static Vector2 SetY(this Vector2 a, float y) => new(a.x, y);
     }
-
-    /// <summary>
-    /// Sets the X axis of a Vector3 to the specified value.
-    /// </summary>
-    /// <param name="a">The original Vector3.</param>
-    /// <param name="x">The new value for the X axis.</param>
-    /// <returns>A new Vector3 with the X axis set to the specified value and the Y and Z axes unchanged.</returns>
-    public static Vector3 SetX(this Vector3 a, float x) => new(x, a.y, a.z);
-
-    /// <summary>
-    /// Sets the Y axis of a Vector3 to the specified value.
-    /// </summary>
-    /// <param name="a">The original Vector3.</param>
-    /// <param name="y">The new value for the Y axis.</param>
-    /// <returns>A new Vector3 with the Y axis set to the specified value and the X and Z axes unchanged.</returns>
-    public static Vector3 SetY(this Vector3 a, float y) => new(a.x, y, a.z);
-
-    /// <summary>
-    /// Sets the Z axis of a Vector3 to the specified value.
-    /// </summary>
-    /// <param name="a">The original Vector3.</param>
-    /// <param name="z">The new value for the Z axis.</param>
-    /// <returns>A new Vector3 with the Z axis set to the specified value and the X and Y axes unchanged.</returns>
-    public static Vector3 SetZ(this Vector3 a, float z) => new(a.x, a.y, z);
-
-    /// <summary>
-    /// Sets the X axis of a Vector2 to the specified value.
-    /// </summary>
-    /// <param name="a">The original Vector2.</param>
-    /// <param name="x">The new value for the X axis.</param>
-    /// <returns>A new Vector2 with the X axis set to the specified value and the Y axis unchanged.</returns>
-    public static Vector2 SetX(this Vector2 a, float x) => new(x, a.y);
-
-    /// <summary>
-    /// Sets the Y axis of a Vector2 to the specified value.
-    /// </summary>
-    /// <param name="a">The original Vector2.</param>
-    /// <param name="y">The new value for the Y axis.</param>
-    /// <returns>A new Vector2 with the Y axis set to the specified value and the X axis unchanged.</returns>
-    public static Vector2 SetY(this Vector2 a, float y) => new(a.x, y);
 }
